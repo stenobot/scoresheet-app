4f4128a033ca31797408a6fdeee2badd66764552
Andy Myers <andrewmyers@gmail.com>
2023-10-03 11:11:32 -0700

    added Settings page, resources, and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Scoresheet from './components/Scoresheet';
 import Start from './components/Start';
 import Settings from './components/Settings';
 
+export enum Preset {
+  None = 'none',
+  ProgressiveRook = 'Progressive Rook'
+}
+
 function App() {
+  const [title, setTitle] = useState('Game 1'); 
+  const [colNum, setColNum] = useState(2);
+  const [showRowNums, setShowRowNums] = useState(false);
+  const [startingRowNum, setStartingRowNum] = useState(1);
+  const [showColTotals, setShowColTotals] = useState(false);
+  const [presets] = useState([Preset.None, Preset.ProgressiveRook]);
+  const [currPreset, setCurrPreset] = useState(Preset.None);
 
   return (
         <Router>
@@ -13,7 +26,19 @@ function App() {
         <Route 
           path="/settings" 
           element={
-            <Settings />} />
+            <Settings 
+              title={title}
+              setTitle={setTitle} 
+              colNum={colNum}
+              setColNum={setColNum}
+              showRowNums={showRowNums}
+              setShowRowNums={setShowRowNums}
+              startingRowNum={startingRowNum}
+              setStartingRowNum={setStartingRowNum}
+              showColTotals={showColTotals}
+              setShowColTotals={setShowColTotals}
+              presets={presets}
+              setCurrPreset={setCurrPreset} />} />
         <Route 
           path="/scoresheet" 
           element={
