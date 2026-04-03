11bbdbcb88447c6698ecf6e33075d6c7a7bd5de8
Andy <andrewmyers@gmail.com>
2026-04-02 23:03:17 -0700

    set up github actions deployment workflow and related settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Start from './components/Start';
 import ProgScoresheet from './components/ProgressiveRook/ProgScoresheet';
